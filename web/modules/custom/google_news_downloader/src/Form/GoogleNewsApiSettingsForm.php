8b96ce0d4c07d8fa90e4b56387cc44fd41e14ccd
Roman <roman.chekhaniuk@smile-ukraine.com>
2022-09-13 23:29:59 +0300

    Creating Google News API, step 2

--- web/modules/custom/google_news_downloader/src/Form/GoogleNewsApiSettingsForm.php
+++ web/modules/custom/google_news_downloader/src/Form/GoogleNewsApiSettingsForm.php
@@ -24,12 +24,13 @@ class GoogleNewsApiSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
+    $config = $this->config('google_news_downloader.settings');
     $form['text'] = [
       '#type' => 'fieldset',
       '#title' => $this->t('Google News API settings'),
     ];
 
-    $google_news_api_key = $this->config('google_news_downloader.settings')->get('google_news_api_key');
+    $google_news_api_key = $config->get('google_news_api_key');
     $form['google_news_api_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('API key'),
@@ -40,7 +41,15 @@ class GoogleNewsApiSettingsForm extends ConfigFormBase {
       '#required' => TRUE,
     ];
 
-    $form['frequency']['top_head_line_frequency'] = [
+    // Settings for "Top headline news".
+    $form['top_head_settings'] = array(
+      '#type' => 'details',
+      '#title' => t('Settings for "Top headline news"'),
+      '#description' => $this->t('Here you can configure news downloader for "Top headline news"'),
+      '#open' => TRUE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
+    );
+
+    $form['top_head_settings']['top_head_line_frequency'] = [
       '#type' => 'select',
       '#title' => $this->t('Select the frequency of top head line news downloading'),
       '#default_value' => $this->config('google_news_downloader.settings')->get('top_head_line_frequency'),
@@ -51,6 +60,60 @@ class GoogleNewsApiSettingsForm extends ConfigFormBase {
         90 => $this->t('Every @time hour', ['@time' => 1.5 ]),
         120 => $this->t('Every @time hour', ['@time' => 2 ]),
       ],
+    ];
+
+    $form['top_head_settings']['countries'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Counties of news'),
+      '#default_value' => $config->get('countries'),
+      '#description' => $this->t('Add country across coma (Example: ua, us).'),
+    ];
+
+    $form['top_head_settings']['category'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Categories of news'),
+      '#default_value' => $config->get('categories'),
+      '#description' => $this->t('Add category across coma (Example: sport, politic).'),
+    ];
+
+    $form['top_head_settings']['q_keys'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Key phrases found in the news'),
+      '#default_value' => $config->get('q_keys'),
+      '#description' => $this->t('Add phrases across coma (Example: fish and meet, bad guys).'),
+    ];
+
+    $form['top_head_settings']['sources'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Sources of news'),
+      '#default_value' => $config->get('sources'),
+      '#description' => $this->t('Add sources across coma (Example: BBC, The Washington Post).'),
+    ];
+
+    $form['top_head_settings']['details'] = array(
+      '#type' => 'details',
+      '#title' => $this->t('Additional'),
+      '#open' => FALSE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
+    );
+
+    $form['top_head_settings']['details']['page_size'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Max count of news in one request'),
+      '#default_value' => $config->get('page_size'),
+    ];
+
+    $form['top_head_settings']['details']['page'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Count of pages with news on one page in one request'),
+      '#default_value' => $config->get('page'),
+    ];
+
+    // Settings for "Everything news".
+    $form['top_head_settings'] = [
+      '#type' => 'details',
+      '#title' => t('Settings for "Everything news"'),
+      '#description' => $this->t('Here you can configure news downloader for "Top headline news"'),
+      '#open' => TRUE, // Controls the HTML5 'open' attribute. Defaults to FALSE.
     ];
 
     $form['frequency']['everything_frequency'] = [
